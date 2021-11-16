9a6e8593e05bdc4445104c96e57ad9d53df97675
zioan <zaharia.ioan@gmail.com>
2021-11-16 12:29:23 +0100

    logo color fix

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -12,7 +12,9 @@
     "
   >
     <div>
-      <p class="p-4 text-lg border-8 w-max md:mx-4">The Movie DB API</p>
+      <p class="p-4 text-lg border-8 w-max md:mx-4 text-gray-100">
+        The Movie DB API
+      </p>
     </div>
     <div id="nav" class="pt-8 pb-4 md:p-0">
       <router-link to="/">Popular</router-link> |
